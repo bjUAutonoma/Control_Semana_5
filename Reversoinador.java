b4a773f2d3b09f5c14211f954ef46cfa42c14efd
bjUAutonoma <163364933+bjUAutonoma@users.noreply.github.com>
2024-04-04 16:59:12 -0300

    Add files via upload

---
+++ Reversoinador.java
@@ -0,0 +1,72 @@
+/**
+ *
+ * @author bjuan
+ */
+
+import java.util.Scanner;
+
+public class Reversoinador {
+
+    public static void main(String[] args) {
+
+        //String trys = "abcdefghijklmnopqrstuvwxyz";
+        //System.out.println(trys.length());
+        //System.out.println(trys.charAt(trys.length()-1));
+        //for (int i=trys.length()-1;i>=0;i--){
+        //    System.out.println(i);
+        //    System.out.println(trys.charAt(i));
+        //}
+        Reversoinador.menu();
+    }
+    
+    public static void menu() {
+        
+        Scanner input = new Scanner(System.in);
+        boolean flag = true;
+        
+        while (flag==true){
+            
+            System.out.print("BIENVENIDO AL REVERSOINADOR"
+                    +"\n"
+                    + "\nCOMO DESEA OPERAR?"
+                    + "\n(1) Ingresar numero"
+                    + "\n(2) Finalizar"
+                    + "\n");
+            
+            int option = input.nextInt();
+            
+            switch (option){
+                
+                case 1:
+                    Reversoinador.input();
+                case 2:
+                    flag=false;
+                    
+            }
+        }
+    }
+    public static void input() {
+        
+        Scanner input = new Scanner(System.in);
+        int num = 0;
+        try {
+            System.out.println("\nIngrese el numero ENTERO a invertir:");
+            num = input.nextInt();
+        }
+        catch (Exception e) {
+            System.out.println("\nNO INGRESO UN NUMERO/NUMERO ENTERO\n");
+            Reversoinador.input();
+        }
+        Reversoinador.reverse(num);
+    }
+    public static void reverse(int num) {
+        
+        String strNum = Integer.toString(num);
+        String reverseNum = new String();
+        for (int i=strNum.length()-1;i>=0;i--){
+            reverseNum = reverseNum+strNum.charAt(i);
+        }
+        System.out.println("\nEl numero invertido es: "+reverseNum+"\n");
+        Reversoinador.menu();
+    }
+}
